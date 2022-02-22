613203b4b5cb6b9a8d547cdb1a0815ce1be3df6f
WooWon Kang <mosiccan@gmail.com>
2022-02-22 22:50:04 +0900

    Feat : first design

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,108 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
- // const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tennis Score System',
-      home: MyHomePage(),
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
+      home: const MyHomePage(title: 'Tennis Score System'),
     );
   }
 }
-//This is YopleNople
-//This is Mosiccan
-class MyHomePage extends StatelessWidget {
-  Widget build(BuildContext context){
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int counter_p1 = 0;
+  int counter_p2 = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      counter_p1++;
+    });
+  }
+
+  void _decrementCounter(){
+    setState(() {
+      counter_p2++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green[200],
       appBar: AppBar(
-        title: Text('Tennis Score System'),
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        elevation: 0.0,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
         child: Column(
-          children: [
-            Text('NAME',
-            style: TextStyle(
-              color: Colors.white,
-              letterSpacing: 2.0,
-            ),
-            ),
-            Text('Tennis',
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Score',
               style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
               ),
             ),
+            SizedBox(height: 30,),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '$counter_p1',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                  SizedBox(width: 30,),
+                  Text(
+                    '$counter_p2',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ]
+            ),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton.extended(
+                    onPressed: _incrementCounter,
+                    tooltip: 'Increment',
+                    icon: Icon(Icons.add),
+                    label: Text("Player 1")
+                ),
+                SizedBox(width: 20,),
+                FloatingActionButton.extended(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  icon: Icon(Icons.add),
+                  label: Text("Player 2"),
+                ),
+              ],
+            )
           ],
         ),
-      )
+      ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
     );
   }
 }
