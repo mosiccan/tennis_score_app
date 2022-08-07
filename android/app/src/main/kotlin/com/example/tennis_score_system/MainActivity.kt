22591d05713223a3fcffaf26c0d7072f6a90af55
yoplenople <junyboy124@gmail.com>
2022-08-07 23:34:12 +0900

    docs: wear os 적용 위한 설정 추가

--- android/app/src/main/kotlin/com/example/tennis_score_system/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tennis_score_system/MainActivity.kt
@@ -4,3 +4,17 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+
+class MainActivity: FlutterActivity(), AmbientMode.AmbientCallbackProvider {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        // Wire up the activity for ambient callbacks
+        AmbientMode.attachAmbientSupport(this)
+    }
+
+    override fun getAmbientCallback(): AmbientMode.AmbientCallback {
+        return FlutterAmbientCallback(getChannel(flutterView))
+    }
+}
